9a19a274c388a67b36e3fe202315a5f43d10d865
Clayton Siby <claytonsiby@gmail.com>
2020-12-20 01:07:08 +0200

    Install dependencies & configure webpack

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+document.querySelector("p").textContent="Hello World!";
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+
+      {
+        test: /\.(png|svg|jpg|gif|jpeg)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+}
