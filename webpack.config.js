83cd41b146c7a2307756f0752f36fefec5d2d996
Clayton Siby <claytonsiby@gmail.com>
2020-12-21 23:31:23 +0200

    Configure stylelint & eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   watch: true,
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
 
       {
         test: /\.(png|svg|jpg|gif|jpeg)$/,
-        use: ['file-loader']
-      }
-    ]
-  }
-}
+        use: ['file-loader'],
+      },
+    ],
+  },
+};
